b81abd359d40ea2bd8f6ac722c86a5626fee2c7d
bullmoose20 <12549033+bullmoose20@users.noreply.github.com.>
2024-05-17 15:35:06 -0400

    testing the roles

--- botlogs/botlogs.py
+++ botlogs/botlogs.py
@@ -16,7 +16,7 @@ class BotLogs(commands.Cog):
         self.bot_uid = bot.user.id
 
     @commands.command()
-    @has_any_role(929900016531828797, 981499667722424390)  # Replace with actual role IDs
+    # @has_any_role(929900016531828797, 981499667722424390)  # Replace with actual role IDs
     async def botlogs(self, ctx, num_lines: int = 50):
         # Constrain the number of lines between 1 and 10000
         num_lines = max(1, min(num_lines, 10000))
@@ -45,6 +45,5 @@ class BotLogs(commands.Cog):
         # Save the filtered output as a text file
         with io.StringIO(filtered_output) as output_file:
             file = File(output_file, filename="bot_logs.txt")
-
             # Send the message with the attachment
             await ctx.send(f"Ran: {command}\nHere are the last {num_lines} lines of {self.bot_name}'s log files (filtered):", file=file)
