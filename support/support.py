5d84cb6e3a1a31f3eec84be400bc1e08d9321b93
David Young <davidy@funkypenguin.co.nz>
2024-06-18 13:19:25 +1200

    donig it
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- support/support.py
+++ support/support.py
@@ -77,7 +77,7 @@ class RedBotCogSupport(commands.Cog):
                     return await ctx.send(f'Could not find a channel with ID {forum_channel_id}.')
 
                 # Construct subject and description with author's display name (nickname)
-                subject = f"{author_display_name} ({message_link.author.name}) needs elf-ssistence. Invoked by {invoker_display_name}"
+                subject = f"{author_display_name} ({message_link.author.name}) needs elf-ssistance. Invoked by {invoker_display_name}"
                 description = f"{message_link.author.mention}, please continue the conversation here.\n\n**Content:** {message_link.content}\n\n**Attachments:**(if any)"
 
                 # Create a thread in the forum channel
