500a616e572bd5298626ba8cb21298a5d49bdd46
Chloe O'Bot <chloe@funkypenguin.co.nz>
2026-05-18 23:31:49 +0000

    Avoid duplicate usernote slash registration

--- elrondradar/__init__.py
+++ elrondradar/__init__.py
@@ -2,9 +2,4 @@ from .elrondradar import ElrondRadar
 
 
 async def setup(bot):
-    cog = ElrondRadar(bot)
-    await bot.add_cog(cog)
-    bot.tree.add_command(cog.usernote_slash)
-    bot.tree.add_command(cog.usernote_add_slash)
-    bot.tree.add_command(cog.usernote_list_slash)
-    bot.tree.add_command(cog.usernote_delete_slash)
+    await bot.add_cog(ElrondRadar(bot))
